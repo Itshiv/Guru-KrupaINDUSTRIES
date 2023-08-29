46291ac16ed78ac59e9a2392d876fb4bb66fe98c
Itshiv <94543321+Itshiv@users.noreply.github.com>
2023-08-29 10:12:55 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,76 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3500,
+    },
+});
+
+// counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 100, 5786, 2500);
+    counter("count3", 0, 1440, 3000);
+    counter("count4", 0, 7110, 3000);
+});
+
+// Our Partner
+var swiper = new Swiper(".our-partner", {
+    slidesPerView: 4, // Display 4 images at a time
+    spaceBetween: 60,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    breakpoints: {
+      767: {
+        slidesPerView: 2, // Change to 2 images at screen width below 767px
+        spaceBetween: 10,
+      },
+      320: {
+        slidesPerView: 1, // Change to 1 image at screen width below 320px
+        spaceBetween: 8,
+      },
+    },
+  });
+  
+  
+
